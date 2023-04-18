f6a3bddf1b4f686e95a444176729ace331143667
Dmitriy Vasnev <trance0id@yandex.ru>
2023-04-18 19:18:40 +0300

    cors: modified cors processing

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -4,13 +4,13 @@ module.exports = (req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
 
   if (method === 'OPTIONS') {
-    res.set('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.set('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.send({ headers: res.headers });
   }
 
   return next();
