65529d43c6ce327d860b81f8acaf84818de66b7f
Dmitriy Vasnev <trance0id@yandex.ru>
2023-04-18 19:36:28 +0300

    cors: added cors module

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const process = require('process');
 const { errors, celebrate, Joi } = require('celebrate');
 
+const cors = require('cors');
 const auth = require('./middlewares/auth');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
@@ -15,13 +16,14 @@ const { linkPattern } = require('./utils/constants');
 const NotFoundError = require('./utils/errors/NotFoundError');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
+// const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors);
+// app.use(cors);
+app.use(cors());
 
 app.use(cookieParser());
 app.use(bodyParser.json());
