a287d78bd901e194ffe6932d01d84c31effe878a
Dmitriy Vasnev <trance0id@yandex.ru>
2023-04-19 13:16:03 +0300

    feature/cors: changed cors mw logics

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -3,9 +3,9 @@ const { DEFAULT_ALLOWED_METHODS } = require('../utils/constants');
 module.exports = (req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
-  const requestReferer = req.headers.referer;
+  const requestOrigin = req.headers.origin;
 
-  res.header('Access-Control-Allow-Origin', requestReferer);
+  res.header('Access-Control-Allow-Origin', requestOrigin);
   res.header('Access-Control-Allow-Credentials', 'true');
 
   if (method === 'OPTIONS') {
